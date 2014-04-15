1492d889fd87f1cac1e0fac9df57baeee237d352
Menesty <ahnezdyuk@gmail.com>
2014-04-15 18:55:03 +0300

    Edit weight

---
+++ tablet/src/main/java/org/menesty/ikea/tablet/dialog/NumberDialog.java
@@ -0,0 +1,20 @@
+package org.menesty.ikea.tablet.dialog;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+public class NumberDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+       /* AlertDialog.Builder adb = new AlertDialog.Builder(getActivity())
+                .setTitle("Problem connecting to internet").setPositiveButton(R.string.reloadData, this)
+                .setMessage(R.string.reloadDataInfo)
+                .setNegativeButton(R.string.settings, this);
+        Dialog dialog = adb.create();
+        dialog.setCanceledOnTouchOutside(false);*/
+
+        return null;
+    }
+}
